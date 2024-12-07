0b5f6e8a4d93ad7ef9dd75dc22688533ab350f03
21anhn <ltnanh21@gmail.com>
2024-12-07 15:56:37 +0700

    [Update] Modify DbContext

--- Socialix/Data/ApplicationDbContext.cs
+++ Socialix/Data/ApplicationDbContext.cs
@@ -1,59 +1,198 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using Socialix.Models;
 
-namespace Socialix.Data
+namespace Socialix.Data;
+
+public partial class ApplicationDbContext : DbContext
 {
-    /// <summary>
-    /// DbContext
-    /// </summary>
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext()
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<Follow> Follows { get; set; }
-        public DbSet<Friendship> Friendships { get; set; }
-        public DbSet<Like> Likes { get; set; }
-        public DbSet<Message> Messages { get; set; }
-        public DbSet<Notification> Notifications { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Config CreatedAt and UpdatedAt
-            modelBuilder.Entity<IBaseEntity>()
-                .Property(e => e.CreatedAt)
-                .HasDefaultValue(DateTime.UtcNow)
-                .HasDefaultValueSql("GETDATE()");
-
-            modelBuilder.Entity<IBaseEntity>()
-                .Property(e => e.UpdatedAt)
-                .HasDefaultValue(DateTime.UtcNow)
-                .HasDefaultValueSql("GETDATE()");
-
-            modelBuilder.Entity<IBaseEntity>()
-                .Property(e => e.UpdatedAt)
-                .ValueGeneratedOnAddOrUpdate();
-        }
-
-        public override int SaveChanges()
-        {
-            var entries = ChangeTracker.Entries()
-                                        .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
-
-            foreach (var entry in entries)
-            {
-                if (entry.State == EntityState.Added)
-                {
-                    ((IBaseEntity)entry.Entity).CreatedAt = DateTime.UtcNow;
-                }
-                ((IBaseEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
-            }
-
-            return base.SaveChanges();
-        }
-
-
     }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<Follower> Followers { get; set; }
+
+    public virtual DbSet<Friendship> Friendships { get; set; }
+
+    public virtual DbSet<Like> Likes { get; set; }
+
+    public virtual DbSet<Message> Messages { get; set; }
+
+    public virtual DbSet<Notification> Notifications { get; set; }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }        
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.CommentId).HasName("PK__Comments__C3B4DFCAB69B4ECA");
+
+            entity.Property(e => e.CommentId).ValueGeneratedNever();
+            entity.Property(e => e.Content).HasColumnType("text");
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.Post).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.PostId)
+                .HasConstraintName("FK__Comments__PostId__6D9742D9");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Comments__UserId__6E8B6712");
+        });
+
+        modelBuilder.Entity<Follower>(entity =>
+        {
+            entity.HasKey(e => e.FollowerId).HasName("PK__Follower__E85940190F0BCE10");
+
+            entity.Property(e => e.FollowerId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.FollowerUser).WithMany(p => p.FollowerFollowerUsers)
+                .HasForeignKey(d => d.FollowerUserId)
+                .HasConstraintName("FK__Followers__Follo__7EC1CEDB");
+
+            entity.HasOne(d => d.User).WithMany(p => p.FollowerUsers)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Followers__UserI__7DCDAAA2");
+        });
+
+        modelBuilder.Entity<Friendship>(entity =>
+        {
+            entity.HasKey(e => e.FriendshipId).HasName("PK__Friendsh__4D531A54B56010B0");
+
+            entity.Property(e => e.FriendshipId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.UserId1Navigation).WithMany(p => p.FriendshipUserId1Navigations)
+                .HasForeignKey(d => d.UserId1)
+                .HasConstraintName("FK__Friendshi__UserI__762C88DA");
+
+            entity.HasOne(d => d.UserId2Navigation).WithMany(p => p.FriendshipUserId2Navigations)
+                .HasForeignKey(d => d.UserId2)
+                .HasConstraintName("FK__Friendshi__UserI__7720AD13");
+        });
+
+        modelBuilder.Entity<Like>(entity =>
+        {
+            entity.HasKey(e => e.LikeId).HasName("PK__Likes__A2922C1422E0ACE4");
+
+            entity.Property(e => e.LikeId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.Comment).WithMany(p => p.Likes)
+                .HasForeignKey(d => d.CommentId)
+                .HasConstraintName("FK__Likes__CommentId__73501C2F");
+
+            entity.HasOne(d => d.Post).WithMany(p => p.Likes)
+                .HasForeignKey(d => d.PostId)
+                .HasConstraintName("FK__Likes__PostId__725BF7F6");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Likes)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Likes__UserId__7167D3BD");
+        });
+
+        modelBuilder.Entity<Message>(entity =>
+        {
+            entity.HasKey(e => e.MessageId).HasName("PK__Messages__C87C0C9C04C857C8");
+
+            entity.Property(e => e.MessageId).ValueGeneratedNever();
+            entity.Property(e => e.Content).HasColumnType("text");
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.Receiver).WithMany(p => p.MessageReceivers)
+                .HasForeignKey(d => d.ReceiverId)
+                .HasConstraintName("FK__Messages__Receiv__7AF13DF7");
+
+            entity.HasOne(d => d.Sender).WithMany(p => p.MessageSenders)
+                .HasForeignKey(d => d.SenderId)
+                .HasConstraintName("FK__Messages__Sender__79FD19BE");
+        });
+
+        modelBuilder.Entity<Notification>(entity =>
+        {
+            entity.HasKey(e => e.NotificationId).HasName("PK__Notifica__20CF2E126EE00242");
+
+            entity.Property(e => e.NotificationId).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.Message).HasColumnType("text");
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Notifications)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Notificat__UserI__019E3B86");
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.PostId).HasName("PK__Posts__AA1260185F490794");
+
+            entity.Property(e => e.PostId).ValueGeneratedNever();
+            entity.Property(e => e.Content).HasColumnType("text");
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.MediaUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Posts__UserId__6ABAD62E");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__1788CC4C44F7E0F3");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D1053488408B0B").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.AvatarPhotoUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Bio).HasColumnType("text");
+            entity.Property(e => e.CoverPhotoUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.CreatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.DoB).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.LastLogin).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasColumnType("decimal(8, 0)");
+            entity.Property(e => e.UserName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
