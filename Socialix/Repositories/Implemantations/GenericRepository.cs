d7770c5ebe62d19d417ce3826d77e5c8a09222c4
21anhn <ltnanh21@gmail.com>
2024-12-14 21:36:41 +0700

    [Add] Define generic repository

---
+++ Socialix/Repositories/Implemantations/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using Socialix.Data;
+using Socialix.Repositories.Interfaces;
+
+namespace Socialix.Repositories.Implemantations
+{
+    /// <summary>
+    /// Generic repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class GenericRepository<T, TKey> : IGenericRepository<T, TKey> where T : class
+    {
+        private ApplicationDbContext _context;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicacte)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
